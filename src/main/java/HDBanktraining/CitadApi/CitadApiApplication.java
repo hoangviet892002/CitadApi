b22fb74b8407d404d1bcfb450dd70a3fcb793718
viet <viethxse@gmail.com>
2024-09-04 09:40:21 +0700

    first commit

---
+++ src/main/java/HDBanktraining/CitadApi/CitadApiApplication.java
@@ -0,0 +1,13 @@
+package HDBanktraining.CitadApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HDBanktraining/CitadApi/CitadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package HDBanktraining.CitadApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
