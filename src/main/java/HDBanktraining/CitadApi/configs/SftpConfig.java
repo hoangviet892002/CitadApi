af695fd5d4ea69f444c7b255a8374caa9d15befa
viet <viethxse@gmail.com>
2024-09-04 13:33:37 +0700

    sftp

---
+++ src/main/java/HDBanktraining/CitadApi/configs/SftpConfig.java
@@ -0,0 +1,31 @@
+package HDBanktraining.CitadApi.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+
+
+@Configuration
+public class SftpConfig {
+    @Value("${spring.application.sftp.host}")
+    private String host = "";
+    @Value("${spring.application.sftp.port}")
+    private int port = 22;
+    @Value("${spring.application.sftp.user}")
+    private String user = "";
+    @Value("${spring.application.sftp.password}")
+    private String password = "";
+
+    @Bean
+    public DefaultSftpSessionFactory sftpSessionFactory() {
+        DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory(true);
+        factory.setHost(host);
+        factory.setPort(port);
+        factory.setUser(user);
+        factory.setPassword(password);
+        factory.setAllowUnknownKeys(true);
+        return factory;
+    }
+
+}
